297c04e1163696cd3c9db4baf43eb2a7b8c5dc17
RACHIT-KUSHWAHA <kushwaharachit80@gmail.com>
2025-11-27 21:23:30 +0530

    Fix root HomeGallery - remove invalid categories prop from GalleryGrid

--- src/components/HomeGallery.tsx
+++ src/components/HomeGallery.tsx
@@ -73,5 +73,5 @@ export function HomeGallery({ initialUserId }: { initialUserId?: string }) {
         }
     }
 
-    return <GalleryGrid photos={photos} categories={categories} />;
+    return <GalleryGrid photos={photos} />;
 }
